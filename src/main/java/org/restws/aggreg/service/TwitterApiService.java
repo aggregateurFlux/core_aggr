1290fa67fec9c4a5abf1f46f767ace7eaf39f054
VictorGD <victor.godier@gmail.com>
2016-12-12 11:51:20 +0100

    commit
    commit

--- src/main/java/org/restws/aggreg/service/TwitterApiService.java
+++ src/main/java/org/restws/aggreg/service/TwitterApiService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.restws.aggreg.model.InstagramAccount;
@@ -17,17 +18,28 @@ import com.google.gson.Gson;
 public class TwitterApiService {
 
 	public static List<Post> getPosts(User user) throws IOException, JSONException {
-		HashMap<String,String> TwitterParams = new HashMap<String,String>();
-		TwitterParams.put("access_token", user.getTwitteraccount().getAccessTokenId());
-		TwitterParams.put("access_token_secret", user.getTwitteraccount().getAccessTokenSecret());
-        String json = HttpService.callMethod("GET", "http://localhost:8012/timeline", TwitterParams);
-		
-		JSONObject jsonObject = new JSONObject( json );
-	    List<Post> list = new ArrayList<Post>();
-	    
-	    //Comment je peux remplir ma liste de posts?
-		
-		return list;
+		 HashMap<String, String> TwitterParams = new HashMap<String, String>();
+	       TwitterParams.put("access_token", user.getTwitteraccount().getAccessTokenId());
+	       TwitterParams.put("access_token_secret", user.getTwitteraccount().getAccessTokenSecret());
+	       String json = HttpService.callMethod("GET", "http://localhost:8012/timeline", TwitterParams); //twitter call
+
+	       List<Post> posts = new ArrayList<Post>();
+
+	       JSONArray jsonArray = new JSONArray(json);
+
+	       for (int i = 0; i < jsonArray.length(); i++) {
+	           JSONObject jsonObject = jsonArray.getJSONObject(i);
+	           
+	           Post post = new Post();
+	           post.setIdAuthor( String.valueOf( jsonObject.get( "idAuthor" ) ) );
+	           post.setAuthorName( String.valueOf( jsonObject.get( "authorName" ) ) );
+	           post.setContent( String.valueOf( jsonObject.get( "content" ) ) );
+	           post.setImageLink( String.valueOf( jsonObject.get("imageLink") ) );
+	           
+	           posts.add( post );
+	       }
+
+	       return posts;
 	}
 	
 	public static String createPost(String content, String access_token_id, String access_token_secret) throws IOException {
